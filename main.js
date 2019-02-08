a1a915678277f55e8da8d0a247cadacc9be77aaa
cswbrian <brianleung1017@gmail.com>
2019-02-09 02:14:03 +0800

    Update logic and test result

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ exports.parseStreetNumber = inputAddress => {
         buildingNoTo: null
     }
 
-    if ((coarseMatch = /[街路道里徑巷坊臺段處圍橋台廊堤線利間][東南西北中]*([\d\s\w\-至及、，,]+?)號/gi.exec(inputAddress)) !== null) {
+    if ((coarseMatch = /[街路道里徑巷坊臺段處圍橋台廊堤線利間][東南西北中]*([\d\s\w\-－—至及、，,]+?)號/gi.exec(inputAddress)) !== null) {
         // console.log(`Coarse match: ${coarseMatch[1]}`)
         let buildingNumbers = []
         while ((fineMatch = streetNoPattern.exec(coarseMatch[1])) !== null) {
